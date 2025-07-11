c44a7f4471b0b2ad4fea481793f796107258476f
Testinfo-01 <testinfo217@gmail.com>
2025-07-10 22:54:59 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function openModal(type) {
+    document.getElementById(type + "Modal").style.display = "block";
+}
+
+function closeModal(type) {
+    document.getElementById(type + "Modal").style.display = "none";
+}
+
+function login() {
+    alert("Fonction de connexion à implémenter avec Firebase.");
+}
+
+function signup() {
+    alert("Fonction d'inscription à implémenter avec Firebase.");
+}
